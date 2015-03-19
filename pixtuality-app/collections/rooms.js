f27029225305b6d5f9825ad2cce11bffce2575e8
met5678 <andrew@luminal.io>
2015-03-18 20:24:31 -0400

    Everything one

--- pixtuality-app/collections/rooms.js
+++ pixtuality-app/collections/rooms.js
@@ -1,1 +1,26 @@
 Rooms = new Meteor.Collection('rooms');
+
+Meteor.methods({
+	'room_create': function() {
+		var room = {
+			mode: 'lobby',
+			participants: [],
+			maxParticipants: 32,
+			phoneNumber:'571-989-4785'
+		};
+		console.log('Create room');
+
+		Rooms.insert(room);
+	}
+});
+
+//console.log(ig);
+/*
+{
+	_id:
+	phoneNumber:
+	mode: 'lobby',
+	participants:
+	maxParticipants:
+}
+*/
