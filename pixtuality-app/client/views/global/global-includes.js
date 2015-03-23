f8269a85c8cf0914072805f1fb010fcc1e7cb3e0
met5678 <andrew@luminal.io>
2015-03-22 22:11:07 -0400

    Updating client stuff

---
+++ pixtuality-app/client/views/global/global-includes.js
@@ -0,0 +1,4 @@
+//FView.registerView('RenderController', famous.views.RenderController);
+//FView.registerView('Scrollview', famous.views.Scrollview);
+
+SpringTransition = famous.transitions.SpringTransition;
